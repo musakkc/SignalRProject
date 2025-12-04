ed42d06d2e2d62f674c5a4594c979bb6206c0de2
Musakc <167858218+musakkc@users.noreply.github.com>
2025-12-04 15:09:13 +0300

    bölüm17 son

---
+++ SignalRApi/Controllers/NotificationController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+        public IActionResult NotificationList()
+        {
+            return Ok(_notificationService.TGetListAll());
+        }
+        [HttpGet("GetNotificationCountByStatusFalse")]
+        public IActionResult GetNotificationCountByStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+        [HttpGet("GetAllNotificationByFalse")]
+        public IActionResult GetAllNotificationByFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByFalse());
+        }
+        [HttpPost]
+        public IActionResult PostNotification(CreateNotificationDto createNotificationDto)
+        {
+            Notification notification = new Notification()
+            {
+                Description = createNotificationDto.Description,
+                Icon = createNotificationDto.Icon,
+                Status = false,
+                Type = createNotificationDto.Type,
+                Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+            };
+            _notificationService.TAdd(notification);
+            return Ok("Ekleme işlemi başarıyla yapıldı");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            _notificationService.TDelete(value);
+            return Ok("Bildirim silindi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetNotification(int id)
+        {
+            var value = _notificationService.TGetById(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+        {
+            Notification notification= new Notification()
+            {
+                NotificationID = updateNotificationDto.NotificationID,
+                Description = updateNotificationDto.Description,
+                Icon = updateNotificationDto.Icon,
+                Status = updateNotificationDto.Status,
+                Type = updateNotificationDto.Type,
+                Date = updateNotificationDto.Date
+            };
+            _notificationService.TUpdate(notification);
+            return Ok("Güncelleme işlemi başarıyla yapıldı");
+        }
+
+        [HttpGet("NotificationStatusChangeToFalse/{id}")]
+        public IActionResult NotificationStatusChangeToFalse(int id)
+        {
+            _notificationService.TNotificationStatusChangeToFalse(id);
+            return Ok("Bildirim durumu okunmadı olarak ayarlandı");
+        }
+
+        [HttpGet("NotificationStatusChangeToTrue/{id}")]
+        public IActionResult NotificationStatusChangeToTrue(int id)
+        {
+            _notificationService.TNotificationStatusChangeToTrue(id);
+            return Ok("Bildirim durumu okundu olarak ayarlandı");
+        }
+
+    }
+}
--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -11,48 +11,52 @@ namespace SignalRApi.Hubs
         private readonly IOrderService _orderService;
         private readonly IMoneyCaseService _moneyCaseService;
         private readonly IMenuTableService _menuTableService;
+        private readonly IBookingService _bookingService;
+        private readonly INotificationService _notificationService;
 
-        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
+        public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService, IBookingService bookingService, INotificationService notificationService)
         {
             _categoryService = categoryService;
             _productService = productService;
             _orderService = orderService;
             _moneyCaseService = moneyCaseService;
             _menuTableService = menuTableService;
+            _bookingService = bookingService;
+            _notificationService = notificationService;
         }
 
         public async Task SendStatistic()
         {
             var value = _categoryService.TCategoryCount();
             await Clients.All.SendAsync("ReceiveCategoryCount", value);
-        
+
             var value2 = _productService.TProductCount();
             await Clients.All.SendAsync("ReceiveProductCount", value2);
-            
+
             var value3 = _categoryService.TActiveCategoryCount();
             await Clients.All.SendAsync("ReceiveActiveCategoryCount", value3);
-        
+
             var value4 = _categoryService.TPassiveCategoryCount();
             await Clients.All.SendAsync("ReceivePassiveCategoryCount", value4);
 
             var value5 = _productService.TProductCountByCategoryNameHamburger();
             await Clients.All.SendAsync("ReceiveTProductCountByCategoryNameHamburger", value5);
-        
+
             var value6 = _productService.TProductCountByCategoryNameDrink();
             await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", value6);
-        
+
             var value7 = _productService.TProductPriceAvg();
             await Clients.All.SendAsync("ReceiveProductPriceAvg", value7.ToString("0.00") + "₺");
-        
+
             var value8 = _productService.TProductNameByMaxPrice();
             await Clients.All.SendAsync("ReceiveProductNameByMaxPrice", value8);
-        
+
             var value9 = _productService.TProductNameByMinPrice();
             await Clients.All.SendAsync("ReceiveProductNameByMinPrice", value9);
-        
+
             var value10 = _productService.TProductAvgPriceByHamburger();
             await Clients.All.SendAsync("ReceiveProductAvgPriceByHamburger", value10.ToString("0.00") + "₺");
-        
+
             var value11 = _orderService.TTotalOrderCount();
             await Clients.All.SendAsync("ReceiveTotalOrderCount", value11);
 
@@ -71,7 +75,7 @@ namespace SignalRApi.Hubs
             var value16 = _menuTableService.TMenuTableCount();
             await Clients.All.SendAsync("ReceiveMenuTableCount", value16);
         }
-        
+
         public async Task SendProgress()
         {
             var value = _moneyCaseService.TTotalMoneyCaseAmount();
@@ -83,6 +87,30 @@ namespace SignalRApi.Hubs
             var value3 = _menuTableService.TMenuTableCount();
             await Clients.All.SendAsync("ReceiveMenuTableCount", value3);
 
+        }
+
+        public async Task GetBookingList()
+        {
+            var values = _bookingService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveBookingList", values);
+        }
+
+        public async Task GetNotificationList()
+        {
+            var value = _notificationService.TNotificationCountByStatusFalse();
+            await Clients.All.SendAsync("ReceiveNotificationCountByFalse", value);
+
+            var notificationListByFalse = _notificationService.TGetAllNotificationByFalse();
+            await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
+        }
+
+        public async Task SendNotification()
+        {
+            var value = _notificationService.TNotificationCountByStatusFalse();
+            await Clients.All.SendAsync("ReceiveNotificationCountByFalse", value);
+
+            var notificationListByFalse = _notificationService.TGetAllNotificationByFalse();
+            await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
         }
     }
 }
